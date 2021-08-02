21f91d2b2b0816e5f6883e87cb9df734faf57b3f
Jean-Sébastien Dupuy <jdupuy@microsoft.com>
2021-08-02 14:35:08 +0000

    Spike - Test with tensorflow base image

--- nlp/main.py
+++ nlp/main.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from transformers import pipeline
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/test")
+def test():
+    classifier = pipeline("sentiment-analysis")
+    result = classifier("I've been waiting for a HuggingFace course my whole life.")
+    return result
