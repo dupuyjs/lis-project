1b4cbf87174418be79975ffa3777f1bdc2b2021d
Jean-Sébastien Dupuy <jdupuy@microsoft.com>
2021-08-05 14:48:59 +0000

    Updating main.py with /sentiment route path

--- nlp/main.py
+++ nlp/main.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 from transformers import pipeline
 
 app = Flask(__name__)
+classifier = pipeline("sentiment-analysis")
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
-@app.route("/test")
+@app.route("/sentiment")
 def test():
-    classifier = pipeline("sentiment-analysis")
-    result = classifier("I've been waiting for a HuggingFace course my whole life.")
-    return tupple(result)
+    args = request.args
+
+    if "sentence" in args:
+        sentence = args.get("sentence")
+        result = classifier(sentence)
+        return result[0], 200
+    else
+        return "No query string received", 200
